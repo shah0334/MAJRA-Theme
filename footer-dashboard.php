8cdd9e4a993d914d42b70481680311e12ddae8f5
Muhammad Shah <shah033499@gmail.com>
2026-01-09 04:24:23 +0500

    clean up dashboard footer file

--- footer-dashboard.php
+++ footer-dashboard.php
@@ -11,12 +11,7 @@
 global $language;
 ?>
 
-        <!-- Dashboard Footer (Hidden/Empty as requested) -->
-        <!-- 
-        <footer class="dashboard-footer">
-            ...
-        </footer>
-        -->
+
 		<?php wp_footer(); ?>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
