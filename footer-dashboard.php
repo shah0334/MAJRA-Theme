853ce355feb1b89289e141109d60b6cfc188a8fe
Muhammad Shah <shah033499@gmail.com>
2026-01-09 04:22:38 +0500

    hide dashboard footer content but keep key scripts

--- footer-dashboard.php
+++ footer-dashboard.php
@@ -11,75 +11,12 @@
 global $language;
 ?>
 
-        <!-- Dashboard Footer -->
+        <!-- Dashboard Footer (Hidden/Empty as requested) -->
+        <!-- 
         <footer class="dashboard-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-lg-4">
-                        <div class="ft-logo"><img class="img-fluid" style="max-width:277px" src="<?php echo get_template_directory_uri();?>/assets/img/footer-logo.png" /></div>
-                        <div class="ft-subs-div text-deep-ocean">
-                            <h3 class="mb-3"><?php echo $language['SUBSCRIBE']['DETAIL_1']; ?></h3>
-                            <?= do_shortcode('[mc4wp_form id=272]'); ?>
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-7 offset-lg-1">
-                        <div class="row">
-                            <div class="col-6 col-md-4 ft-links">
-                                <?php
-                                    wp_nav_menu(
-                                        array(
-                                            'theme_location' => 'footer-menu-1',
-                                            'menu_class'     => '', 
-                                        )
-                                    );
-                                    ?>
-                            </div>
-                            <div class="col-6 col-md-4 ft-links">
-                                <?php
-                                    wp_nav_menu(
-                                        array(
-                                            'theme_location' => 'footer-menu-2',
-                                            'menu_class'     => '', 
-                                        )
-                                    );
-                                ?>
-                                <div class="d-flex flex-column gap-3 mt-3">
-                                    <a href="tel:800277823" class="d-flex align-items-center gap-2 fs-6 text-decoration-none text-primary fw-semibold">
-                                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/phone.png" />
-                                        <p class="m-0" dir="ltr">800 277823</p>
-                                    </a> 
-                                    <a href="mailto:info@uaemajra.ae" class="d-flex align-items-center gap-2 fs-6 text-decoration-none text-primary fw-semibold">
-                                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/mail.png" />
-                                        <p class="m-0">info@uaemajra.ae</p>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-12 col-md-4 ft-links">
-                                <?php
-                                        wp_nav_menu(
-                                            array(
-                                                'theme_location' => 'footer-menu-3',
-                                                'menu_class'     => '', 
-                                            )
-                                        );
-                                        ?>
-                                        <div class="ft-socials">
-                                            <h3><?php pll_e('Follow Majra'); ?></h3>
-                                            <div class="ft-soc-row">
-                                                <a target="_blank" href="https://www.instagram.com/uaemajra/"><img src="<?php bloginfo('template_directory'); ?>/assets/img/ft-insta.svg" /></a> 
-                                                <a target="_blank" href="https://www.linkedin.com/company/uaemajra/"><img src="<?php bloginfo('template_directory'); ?>/assets/img/ft-linkedin.svg" /></a>
-                                                <a target="_blank" href="https://twitter.com/uaemajra"><img src="<?php bloginfo('template_directory'); ?>/assets/img/ft-x.svg" /></a>
-                                            </div>
-                                        </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row copyright-row">
-                    <div class="col-md-12"><p class="copyright"><?php pll_e('Copyright © 2025 National CSR Fund Majra'); ?></p></div>
-                </div>
-            </div>
+            ...
         </footer>
+        -->
 		<?php wp_footer(); ?>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
